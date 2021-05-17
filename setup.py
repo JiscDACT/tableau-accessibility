64de67e6b2c8ddacb7619a3808c71be12de1aefa
scottbw <scott.bradley.wilson@gmail.com>
2021-05-17 17:54:57 +0100

    Setting up CI

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+VERSION = '1.0.0'
+
+setup(
+    name='tabfix',
+    version=VERSION,
+    py_modules=['tabfix'],
+    url='https://github.com/scottbw/tabfix',
+    download_url='https://github.com/scottbw/tabfix/tarball/{}'.format(VERSION),
+    license='MIT',
+    author='Scott Wilson',
+    author_email='scott.bradley.wilson@gmail.com',
+    description='A command-line tool for improving Tableau accessibility',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=['lxml==4.6.2', 'pyyaml==5.4.1'],
+    entry_points={
+        'console_scripts': ['tabfix=tabfix:main'],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
